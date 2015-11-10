287fdf1db281b4fe58c53fe429d03073d4ef27b5
Abey Onalaja <abey.onalaja@gmail.com>
2015-11-10 17:23:10 +0000

    Add inital model

--- Bingo.elm
+++ Bingo.elm
@@ -5,6 +5,25 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (toUpper, repeat, trimRight)
 
+newEntry phrase points id =
+  {
+    phrase    = phrase,
+    points    = points,
+    wasSpoken = False,
+    id        = id
+  }
+
+initialModel =
+  {
+    entries =
+      [
+       newEntry "Doing Agile" 200 2,
+       newEntry "In the cloud" 300 3,
+       newEntry "Future-Proof" 100 1
+      ]
+  }
+
+
 title message times =
   message ++ " "
   |> toUpper
@@ -23,27 +42,27 @@ pageFooter =
        [ text "The Pragmatuc Studio"]]
 
 
-entryItem phrase points =
+entryItem entry =
   li [ ]
      [
-      span [class "phrase"] [text phrase],
-      span [class "points"] [text (toString points)]
+      span [class "phrase"] [text entry.phrase],
+      span [class "points"] [text (toString entry.points)]
      ]
 
 entryList =
   ul [ ]
      [
-      entryItem "Future-Proof" 100,
-      entryItem "Doing Agile" 200
+      entryItem ( newEntry "Future-Proof" 100 1),
+      entryItem ( newEntry "agile" 200 2)
      ]
 
     
-view =
+view model =
   div [id "container"]
       [ pageHeader,
-        entryList,
+        entryList model.entries,
         pageFooter ]
 
       
 main =
-  view
+  view initalModel
